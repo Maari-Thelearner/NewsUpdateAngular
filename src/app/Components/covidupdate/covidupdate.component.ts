aa766f20e691b1016e632218a0c018275d124f45
Maari-Thelearner <bs.mari100@gmail.com>
2021-08-27 18:20:19 +0530

    ProjectSetup

---
+++ src/app/Components/covidupdate/covidupdate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-covidupdate',
+  templateUrl: './covidupdate.component.html',
+  styleUrls: ['./covidupdate.component.css']
+})
+export class CovidupdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Components/home/home.component';
+import { BookmarkComponent } from './Components/bookmark/bookmark.component';
+import { CovidupdateComponent } from './Components/covidupdate/covidupdate.component';
+import { AboutusComponent } from './Components/aboutus/aboutus.component';
+import { LoginComponent } from './Components/Authentication/login/login.component';
+import { RegisterComponent } from './Components/Authentication/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookmarkComponent,
+    CovidupdateComponent,
+    AboutusComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
